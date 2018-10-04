57f8559388b9f57e2ac3748821fff0c4804a417f
Suheyl Turac <skbsturac@live.com>
2018-10-04 15:24:27 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+collection == [1, 2, 3, 4]
+
+def my_each(collection) # put argument(s) here
   # code here
+  i = 0
+    while i < array.length
+    yield(collection)
+    i = i + 1
+  end
+    puts i
 end
+  
