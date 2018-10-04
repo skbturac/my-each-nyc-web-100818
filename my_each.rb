c902ec7f6f4c4a2ed7568a4ea8014be92858b970
Suheyl Turac <skbsturac@live.com>
2018-10-04 15:28:45 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
+def my_each(array) 
+  # put argument(s) here
   # code here
-end
+  
+    if block_given?
+    i = 0 
+      while i < array.length 
+        yield (array[i])
+    i = i +  1
+      end
+    array 
+  else
+    
+  end
+    
+  end
+  
